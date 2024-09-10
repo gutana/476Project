c0fbb02c5c1039f1a0b0e2f9b3b1d16dcf14c8e1
Mohammad Albayati <Albayatm@outlook.com>
2024-09-09 19:55:11 -0600

    simplify spinner? i guess??

--- ui/src/components/LoadingSpinner.tsx
+++ ui/src/components/LoadingSpinner.tsx
@@ -2,10 +2,10 @@ import { Spinner, Stack } from "react-bootstrap"
 
 export const LoadingSpinner = () => {
     return (
-        <Stack direction="vertical" className="mt-5">
-            <div className="d-flex justify-content-center margin-top-20px mt-5">
+        <div className="d-flex justify-content-center mt-5">
+            <div className="d-flex justify-content-center mt-5">
                 <Spinner />
             </div>
-        </Stack>
+        </div>
     )
 }
