c65a94bc4f62ac19153f3b49a74e2efeb26ce37f
Mohammad Albayati <Albayatm@outlook.com>
2024-11-10 10:34:13 -0600

    update gitignore, hide swagger for api

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -14,8 +14,8 @@ builder.Services.AddControllers()
     });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
@@ -49,13 +49,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
