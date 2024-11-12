f2312f2ba13db72b67cb1117da16bc846a43c69b
Mohammad Albayati <Albayatm@outlook.com>
2024-11-12 14:54:19 -0600

    styling fix on newscard for short news posts

--- ui/src/pages/Home/components/NewsCard.tsx
+++ ui/src/pages/Home/components/NewsCard.tsx
@@ -16,7 +16,7 @@ export const NewsCard = ({ Title, Content, Date, MaxChars }: Props) => {
     const displayContent = isExpanded ? Content : Content.slice(0, MaxChars);
 
     return (
-        <Card style={{ margin: 'auto', marginTop: '10px', borderRadius: '0' }}>
+        <Card style={{ margin: 'auto', marginTop: '10px', borderRadius: '0', minWidth: '100%' }}>
             <Card.Body>
                 <Card.Title>{Title}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{Date}</Card.Subtitle>
